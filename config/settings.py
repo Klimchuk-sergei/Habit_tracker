b9b868e6d1238a7b41ee68a4eff3d852af668d25
Klimchuk-sergei <klim.work@mail.ru>
2025-10-14 20:34:20 +0300

    add settings database

--- config/settings.py
+++ config/settings.py
@@ -70,8 +70,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME', 'habit_tracker'),
+        'USER': os.getenv('DB_USER', 'postgres'),  # <- Изменил здесь
+        'PASSWORD': os.getenv('DB_PASSWORD', ''),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
