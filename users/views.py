e4510755346d7050de4a5ade97c9dd2993fb8e5e
Klimchuk-sergei <klim.work@mail.ru>
2025-10-16 16:30:27 +0300

    user views done

--- users/views.py
+++ users/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from django.contrib.auth.models import User
+from .serializers import UserSerializer, UserRegistrationSerializer
 
-# Create your views here.
+class UserRegistrationView(generics.CreateAPIView):
+    serializer_class = UserRegistrationSerializer
+    queryset = User.objects.all()
+
+    def create(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exeption=True)
+        user = serializer.save()
+        return Response(
+            {
+                "message": "пользователь успешкно зарегистрирован",
+                "user": UserSerializer(user).data
+            },
+            status=status.HTTP_201_CREATED
+        )
+
