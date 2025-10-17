d2c374d62ba2b3ff20e8d972b148def3f2508ba1
Klimchuk-sergei <klim.work@mail.ru>
2025-10-17 15:11:21 +0300

    create user model. makemigrations

--- users/models.py
+++ users/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Profile(models.Model):
+    """профиль пользователя с доп полями"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE, verbose_name='пользователь')
+    telegram_chat_id = models.CharField(
+        max_length=20,
+        blank=True,
+        null=True,
+        verbose_name='ID чата в ТГ'
+    )
+
+    def __str__(self):
+        return f'профиль{self.user.username}'
+
+    class Meta:
+        verbose_name = 'профиль'
+        verbose_name_plural = 'профили'
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
+
+
